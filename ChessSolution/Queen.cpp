ed7d90568c10cb6ecd68c6a01873a22ef2a03fef
Amit Atal <amitatal1977@gmail.com>
2023-12-24 17:27:57 +0200

    King  and queen classes

--- ChessSolution/Queen.cpp
+++ ChessSolution/Queen.cpp
@@ -1,6 +1,7 @@
 #include "Queen.h"
 
 Queen::Queen(char type, bool color, int x, int y, Board& board)
+    : Piece(type,color,x,y,board)
 {
 }
 
@@ -10,5 +11,32 @@ Queen::~Queen()
 
 bool Queen::IsMovePossible(int x, int y, bool threatSearch)
 {
+
+    if (x < 0 || y < 0 || x >= BOARD_DIMENSION || y >= BOARD_DIMENSION) // out of bounds 
+    {
+        return false;
+    }
+
+    if (_board[x][y] != nullptr && _board[x][y]->getColor() == _color)
+    {
+        // Destination is occupied by a piece of the same color
+        return false;
+    }
+
+    if (!threatSearch && _board.wouldMoveCauseCheck(this, x, y))
+    {
+        return false;
+    }
+
+    int deltaX = std::abs(x - _x);
+    int deltaY = std::abs(y - _y);
+
+    // Queen can move horizontally, vertically, or diagonally
+    if (deltaX == 0 || deltaY == 0 || deltaX == deltaY)
+    {
+        return true;
+    }
+
     return false;
-}
+    
+   }
